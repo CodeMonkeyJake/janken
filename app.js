73306fa043bba0a3f7fe33a32f5d8ac45402d79b
jacob cash <jpcashta@outlook.com>
2021-07-05 20:22:17 -0500

    initial commit

--- app.js
+++ app.js
@@ -1,29 +1,78 @@
 
 
-function computerPlay() {
+
+const playerSelection = prompt('choose "Rock", "Paper" or "Scissors"');
+
+
+
+
+const computerSelection = function () {
 let randomNumber = Math.floor(Math.random() * 3);
 switch(randomNumber) {
     case 0: 
     return 'ROCK';
     case 1: 
-    return "PAPER";
+    return 'PAPER';
     case 2: 
-    return "SCISSORS"
+    return 'SCISSORS';
 
 }
 
 }
+
+
 
 
 function playRound(playerSelection, computerSelection) {
-
-    if (playerSelection = 'ROCK' && computerSelection === 'SCISSORS') {
-    return 'you win!'
+    if (playerSelection === computerSelection) {
+        return "you tie"
     }
-    
+
+    if (
+        (playerSelection === 'ROCK' || 'rock' || 'Rock' && computerSelection === 'SCISSORS') ||
+        (playerSelection === 'PAPER' || 'paper' || 'Paper' & computerSelection === 'ROCK') ||
+        (playerSelection === 'SCISSORS' && computerSelection === 'PAPER')
+    ) {
+    return 'you win!';
+    }  
+      if (
+        (playerSelection === 'ROCK' && computerSelection === 'PAPER') ||
+        (playerSelection === 'PAPER' && computerSelection === 'SCISSORS') ||
+        (playerSelection === 'SCISSORS' && computerSelection === 'ROCK')
+    ) {
+        return 'you lose!';
+    }
 
 }
 
 
-const playerSelection = 'ROCK'
-const computerSelection = computerPlay()
+const game = function() {
+let i = 0
+    while (i < 5) {
+        
+    }
+}
+
+
+
+// function getWinner(playerSelection, computerSelection) {
+//     if (playerSelection === computerSelection) {
+//       return "tie";
+//     }
+//     if (
+//       (playerSelection === "ROCK" && computerSelection === "SCISSORS") ||
+//       (playerSelection === "SCISSORS" && computerSelection === "PAPER") ||
+//       (playerSelection === "PAPER" && computerSelection === "ROCK")
+//     ) {
+//       return "player";
+//     }
+//     if (
+//       (computerSelection === "ROCK" && playerSelection === "SCISSORS") ||
+//       (computerSelection === "SCISSORS" && playerSelection === "PAPER") ||
+//       (computerSelection === "PAPER" && playerSelection === "ROCK")
+//     ) {
+//       return "computer";
+//     }
+//   }
+
+
